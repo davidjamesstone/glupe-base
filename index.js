fd9721de229b26138dd02bf5020ed7be74ff31fd
David Stone <davidjamesstone@gmail.com>
2016-04-23 23:46:41 +0100

    Updated startup

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const Glupe = require('glupe')
 const config = require('./config')
+const appName = require('./package.json').name
 
 Glupe.compose(__dirname, config, function (err, server) {
   if (err) {
     throw err
   }
 
+  /*
+   * Start the server
+   */
   server.start(function (err) {
-    if (err) {
-      server.log('error', 'Error starting server')
-      throw err
+    var details = {
+      name: appName,
+      info: server.info
     }
 
-    server.log('info', 'Server started')
-    console.info('Server running at:', server.info)
+    if (err) {
+      details.error = err
+      details.message = 'Failed to start ' + details.name
+      server.log('error', details)
+      throw err
+    } else {
+      details.config = config
+      details.message = 'Started ' + details.name
+      server.log('info', details)
+      console.info('Server running at:', server.info)
+    }
   })
 })
