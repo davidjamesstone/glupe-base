9fb6a5e787275d0a5bc1f7299d12d96595e20cfb
David Stone <davidjamesstone@gmail.com>
2016-04-23 22:06:14 +0100

    Updated startup and readme

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ Glupe.compose(__dirname, config, function (err, server) {
     throw err
   }
 
-  server.start(function () {
+  server.start(function (err) {
+    if (err) {
+      server.log('error', 'Error starting server')
+      throw err
+    }
+
     server.log('info', 'Server started')
     console.info('Server running at:', server.info)
   })
--- readme.md
+++ readme.md
@@ -1,4 +1,1 @@
-# defcon
-
-##
--
+# glupe-base
